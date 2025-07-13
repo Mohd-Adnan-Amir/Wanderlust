8c703ac17cfba3b500dbdb9fc3d3843c24d5c6b6
Mohd Adnan Amir <mohdadnan0211@gmail.com>
2025-07-13 13:02:33 +0530

    feat: add CRUD routes (Index, Read, Create, Update, Delete)

--- app.js
+++ app.js
@@ -1,27 +1,77 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const Listing = require('./models/listing.js');
+const path = require('path');
+const methodOverride = require('method-override')
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 main()
-.then(() => {
-    console.log("connection succesful")
-})
-.catch(err => console.log(err));
+    .then(() => {
+        console.log("connection succesful")
+    })
+    .catch(err => console.log(err));
 async function main() {
-  await mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL);
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 
 
 //Routes-----
 app.get("/listings", async (req, res) => {
-    const allListings = await Listing.find({}).then( (res) => {
-        res.render("index.js", {allListings})
-    })
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings })
+});
+
+app.get("/listings/new", async (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing })
+});
+ 
+app.post("/listings", async (req, res) => {
+    // let {title, description, image,price, location, country} = req.body;
+    // let listing = req.body;
+    const newListing = new Listing(req.body.listing)
+     await newListing.save();
+   res.redirect("/listings");
+});
+
+
+
+
+
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+
+    res.render("listings/edit.ejs", {listing});
+});
+
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect("/listings")
+    
+});
+
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing)
+    res.redirect("/listings")
+    
 });
 
 
