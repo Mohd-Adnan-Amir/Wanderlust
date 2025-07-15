8045a3f56ed7f03c10613ccc7d74e4ba74fdb472
Mohd Adnan Amir <mohdadnan0211@gmail.com>
2025-07-15 16:43:14 +0530

    Add catch-all route and centralized error handler for undefined routes

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const mongoose = require('mongoose');
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const Listing = require('./models/listing.js');
 const path = require('path');
-const methodOverride = require('method-override')
-const ejsMate = require("ejs-mate");
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const wrapAsync = require('./utils/wrapAsync.js');
+const ExpressError = require('./utils/ExpressError.js');
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
@@ -29,59 +32,58 @@ async function main() {
 //Routes-----
 
 //index route
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync (async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings })
-});
+}));
 
 //new route
-app.get("/listings/new", async (req, res) => {
+app.get("/listings/new", wrapAsync (async (req, res) => {
     res.render("listings/new.ejs");
-});
+}));
 
 //show route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync (async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing })
-});
+}));
 
-//route for update
-app.post("/listings", async (req, res) => {
-    // let {title, description, image,price, location, country} = req.body;
-    // let listing = req.body;
+//create routes
+app.post("/listings", wrapAsync (async (req, res, next) => {
+    if(!req.body.listing){
+        throw new ExpressError(400, "Send valid data for listings");
+    }
     const newListing = new Listing(req.body.listing)
     await newListing.save();
     res.redirect("/listings");
-});
+    
+   })
+);
 
 //edit route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync (async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
 
     res.render("listings/edit.ejs", { listing });
-});
+}));
 
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync (async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect("/listings")
 
-});
+}));
 
 //delete route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync (async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     // console.log(deletedListingnpm)
     res.redirect("/listings")
 
-});
-
-
-
-
+}));
 
 
 //initial route
@@ -89,9 +91,15 @@ app.get("/", (req, res) => {
     res.send("Server Working/ Browser on")
 })
 
+app.all("/*splat", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
+});
+
+// Centralized Error Handler
 app.use((err, req, res, next) => {
-    res.stat
-})
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("error.ejs", { message }); // You can change this to render an EJS view
+});
 app.listen(8080, () => {
     console.log("Server Running");
 })
