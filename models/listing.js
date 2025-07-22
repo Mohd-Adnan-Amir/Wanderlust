5bfa8cf43da12af411aa0f8884002ede8752e415
Mohd Adnan Amir <mohdadnan0211@gmail.com>
2025-07-22 23:53:58 +0530

    Add reference to Review schema in Listing model

--- models/listing.js
+++ models/listing.js
@@ -1,3 +1,4 @@
+const { ref } = require('joi');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const listingSchema = new Schema({
@@ -19,6 +20,12 @@ const listingSchema = new Schema({
     price: Number,
     location: String,
     country: String,
+    review: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
