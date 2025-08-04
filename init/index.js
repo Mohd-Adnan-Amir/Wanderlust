9eabdfd5005cb5f5c4880670caaba90dd43d1710
Mohd Adnan Amir <mohdadnan0211@gmail.com>
2025-08-04 15:25:10 +0530

    Added owner field to initial database

--- init/index.js
+++ init/index.js
@@ -16,6 +16,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "688f3ea874e5b8db53054336"}) )
     await Listing.insertMany(initData.data)
     console.log("data was initialised")
 };
