9e5e42013d40c6b5e6db79046026afe19c1f075c
Mohd Adnan Amir <mohdadnan0211@gmail.com>
2025-08-04 15:19:34 +0530

    Authorization middlewares

--- middleware.js
+++ middleware.js
@@ -1,9 +1,14 @@
+const Listing = require('./models/listing.js');
+const { listingSchema, reviewSchema } = require("./schema.js")
+const Review = require('./models/review.js');
+const ExpressError = require('./utils/ExpressError.js');
+
+
 module.exports.isloggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.redirectUrl = req.originalUrl;
-        req.flash("error", "You must be logged in to add new listing");
+        req.flash("error", "You must be logged in");
         return res.redirect("/login");
-
     }
     next();
 }
@@ -12,5 +17,46 @@ module.exports.savedRedirectUrl = (req, res, next) => {
     if (req.session.redirectUrl) {
         res.locals.redirectUrl = req.session.redirectUrl;
     }
-    next()
+    next();
+}
+
+module.exports.isOwner = async (req, res, next) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    if(!listing.owner._id.equals(res.locals.currUser._id)){
+        req.flash("error", "You dont have permission to do this.")
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+module.exports.validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    // console.log(result);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(", ");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+}
+
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    // console.log(result);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(", ");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    let { id, reviewId } = req.params;
+    let review = await Review.findById(reviewId);
+    if(!review.author.equals(req.user)){
+        req.flash("error", "You are not the author of this review.")
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
 }
